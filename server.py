0b442318c9513c35f4f84e2d4b59ef9673138afd
zwisler <alex-z-@web.de>
2026-07-21 13:27:25 +0200

    Sessionless

--- server.py
+++ server.py
@@ -29,6 +29,7 @@ mcp = FastMCP(
     "garmin-connect",
     host=os.getenv("MCP_HOST", "0.0.0.0"),
     port=int(os.getenv("MCP_PORT", "8000")),
+    stateless_http=True
 )
 
 _client: Garmin | None = None
